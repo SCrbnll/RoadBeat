8cb9363e4534450b8e7bb814c0d5a0fce2f8d011
Samuel Carbonell <120050196+SCrbnll@users.noreply.github.com>
2024-04-11 17:59:38 +0200

    Creada bottomTabNavigator

--- App.js
+++ App.js
@@ -1,19 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, SafeAreaView } from 'react-native';
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import Navigation from './src/routes/StackNavigator';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Navigation />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000000',
     alignItems: 'center',
     justifyContent: 'center',
   },
